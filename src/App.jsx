e6b60278357a83f2394a077602bb924785f6b78d
Sunil Choudhary <msunilkchoudhary@gmail.com>
2025-11-25 14:50:11 +0530

    error solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+
 import { Outlet } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
 function App() {
